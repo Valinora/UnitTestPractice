7ee0f3d1bc0cc0bd61ac8bd724895b36e7cd0e41
Colin Jamison <colin.jamison@liatrio.com>
2025-11-05 11:30:40 -0800

    test: add more unit tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,37 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+
+TEST(PasswordTest, multiple_leading_chars)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aaaaBcD");
+	ASSERT_EQ(4, actual);
+}
+
+TEST(PasswordTest, single_leading_char)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aBcD");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, alternating_case)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aaAABcD");
+	ASSERT_EQ(2, actual);
+}
+
+TEST(PasswordTest, single_case) {
+	Password my_password;
+	bool actual = my_password.has_mixed_case("aaaaaaa");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, mixed_case) {
+	Password my_password;
+	bool actual = my_password.has_mixed_case("AaAaA");
+	ASSERT_EQ(true, actual);
+}
